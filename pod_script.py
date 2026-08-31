9ce2d345e727919209b4b3a3c9cee70d1c54d360
Matrix <55624562+MatrixZockt@users.noreply.github.com>
2026-08-31 20:24:49 +0200

    Change DEFAULT_BLUEPRINT_ID to a static value
    Replace dynamic retrieval of PRINTIFY_BLUEPRINT_ID with a static value.

--- pod_script.py
+++ pod_script.py
@@ -54,7 +54,7 @@ def _get_env_int(var_name: str, default: int) -> int:
     val = os.environ.get(var_name)
     return int(val) if val and val.strip() else default
 
-DEFAULT_BLUEPRINT_ID = _get_env_int("PRINTIFY_BLUEPRINT_ID", 1268)
+DEFAULT_BLUEPRINT_ID = 1268
 DEFAULT_PRINT_PROVIDER_ID = _get_env_int("PRINTIFY_PRINT_PROVIDER_ID", 1)
 
 variant_env = os.environ.get("PRINTIFY_VARIANT_IDS")
